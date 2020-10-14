322990aeea36a39222928b3c369014743ff346fe
Perséides45 <maas.enzo45@gmail.com>
2020-10-14 07:00:30 +0200

    dark mode + header update + format using Prettier

--- app.js
+++ app.js
@@ -15,3 +15,40 @@ $("#navigation-advice").modal("show").on("shown.bs.modal", () => {
         $("#navigation-advice").modal("hide");
     }, 10000);
 });
+
+
+
+let darkModeButton = document.getElementById("dark-mode-button");
+
+let navbarContainer = document.getElementById("navbar-container");
+let navbar = document.getElementById("navbar");
+
+let navLink = document.getElementsByClassName("nav-link");
+
+darkModeButton.addEventListener("click", function () {
+    if (navbarContainer.classList.contains("bg-dark")) {
+        setToLightMode();
+
+    } else {
+        setToDarkMode();
+    }
+});
+
+function setToLightMode() {
+    navbarContainer.classList.add("bg-light")
+    navbarContainer.classList.remove("bg-dark")
+    navbar.classList.add("navbar-light")
+    navbar.classList.remove("navbar-dark")
+    $('.nav-link').addClass('text-dark')
+    $('.nav-link').removeClass('text-light')
+
+}
+
+function setToDarkMode() {
+    navbarContainer.classList.add("bg-dark")
+    navbarContainer.classList.remove("bg-light")
+    navbar.classList.add("navbar-dark")
+    navbar.classList.remove("navbar-light")
+    $('.nav-link').addClass('text-light')
+    $('.nav-link').removeClass('text-dark')
+}   
